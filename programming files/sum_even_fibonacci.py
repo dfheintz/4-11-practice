2835a9a6d0e73daa4a096fe234fcbf61b6d8a9b1
amandabarth <112357592+amandabarth@users.noreply.github.com>
2024-04-11 16:45:54 -0400

    Update sum_even_fibonacci.py

--- programming files/sum_even_fibonacci.py
+++ programming files/sum_even_fibonacci.py
@@ -1,8 +1,12 @@
+#base cases
 a, b = 0, 1
 sum_even = 0
+#loop through first 4000000 terms of fibonacci sequence
 while b < 4000000:
+    #if even add to sum
     if b % 2 == 0:
         sum_even += b
+    #go to next fibonacci number
     a, b = b, a+b
 
 print(sum_even)
